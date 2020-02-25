f6281d4e40f628392cfbe10b6124fbee82ce324c
imtiaz <programmer.imtiaz@gmail.com>
2020-02-26 00:37:57 +0600

    fragment transition added for bttom view

--- app/src/main/java/com/example/bottombarwithnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottombarwithnavigation/MainActivity.kt
@@ -1,14 +1,25 @@
-
 package com.example.bottombarwithnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_main)
+    private lateinit var navController: NavController
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setUpbottomNavigationWithNavController()
     }
+
+    private fun setUpbottomNavigationWithNavController(){
+        navController = findNavController(R.id.navHostFragment)
+        bottomNavView.setupWithNavController(navController)
+    }
+
 }
--- app/src/main/java/com/example/bottombarwithnavigation/MonsterFragment.kt
+++ app/src/main/java/com/example/bottombarwithnavigation/MonsterFragment.kt
@@ -5,14 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
+import kotlinx.android.synthetic.main.fragment_monster.*
 
 class MonsterFragment : Fragment() {
 
-  override fun onCreateView(
-      inflater: LayoutInflater,
-      container: ViewGroup?,
-      savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_monster, container, false)
-  }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_monster, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        feedMePizza.setOnClickListener {
+            findNavController().navigate(R.id.navigation_pizza, null, getNavOptions())
+        }
+    }
+
+    fun getNavOptions() : NavOptions =  navOptions {
+        anim {
+            enter = R.anim.slide_in_right
+            exit = R.anim.slide_out_left
+            popEnter = R.anim.slide_in_left
+            popExit = R.anim.slide_out_right
+        }
+    }
 }
