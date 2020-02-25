ddc8da2a0f38424770784b7497497a6acce34bd5
imtiaz <programmer.imtiaz@gmail.com>
2020-02-25 22:58:52 +0600

    All package name changed

--- app/src/main/java/com/example/bottombarwithnavigation/MainActivity.kt
+++ app/src/main/java/com/example/bottombarwithnavigation/MainActivity.kt
@@ -1,15 +1,14 @@
 
-package com.raywenderlich.feedmenav
+package com.example.bottombarwithnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.bottombarwithnavigation.R
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_main)
 
-  }
+    }
 }
