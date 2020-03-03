c0ce5eb94e3060627f10e6a78607ba41c4ae0949
imtiaz <programmer.imtiaz@gmail.com>
2020-03-03 07:55:23 +0600

    Navigation animation added and argument passing through animation

--- app/src/main/java/com/example/bottombarwithnavigation/NavToChooseRecepientFragment.kt
+++ app/src/main/java/com/example/bottombarwithnavigation/NavToChooseRecepientFragment.kt
@@ -5,15 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_nav_to_choose_recepient.*
 
-class NavToChooseRecepientFragment : Fragment() {
+class NavToChooseRecepientFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    lateinit var navController: NavController
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_nav_to_choose_recepient, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initInstance(view)
+        initListener()
+    }
+
+    private fun initListener() {
+        nextBtn.setOnClickListener(this)
+        cancelBtn.setOnClickListener(this)
+    }
+
+    private fun initInstance(view: View) {
+        navController = Navigation.findNavController(view)
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            when (v.id){
+                R.id.nextBtn ->{
+                    val bundle = bundleOf("recipient" to input_recipient.text.toString())
+                    navController.navigate(R.id.action_navToChooseRecepientFragment_to_navToSepecifyAmountFragment, bundle)
+                }
+                R.id.cancelBtn -> activity?.onBackPressed()
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/bottombarwithnavigation/NavToSepecifyAmountFragment.kt
+++ app/src/main/java/com/example/bottombarwithnavigation/NavToSepecifyAmountFragment.kt
@@ -1,19 +1,54 @@
 package com.example.bottombarwithnavigation
 
 import android.os.Bundle
+import android.os.IBinder
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_nav_to_sepecify_amount.*
 
-class NavToSepecifyAmountFragment : Fragment() {
+class NavToSepecifyAmountFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    lateinit var navController: NavController
+    var recipient: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = arguments?.getString("") ?: ""
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_nav_to_sepecify_amount, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initInstance(view)
+        initListener()
+
+
+    }
+
+    private fun initInstance(view: View) {
+        navController = Navigation.findNavController(view)
+    }
+
+    private fun initListener(){
+        sendBtn.setOnClickListener(this)
+        cancelBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if(v != null){
+            when(v.id){
+                R.id.cancelBtn -> activity?.onBackPressed()
+                R.id.sendBtn -> {navController.navigate(R.id.action_navToSepecifyAmountFragment_to_navToConfirmationFragment, null)}
+            }
+        }
     }
 
 }
